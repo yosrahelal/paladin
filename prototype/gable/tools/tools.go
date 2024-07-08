5d5b5f10f3fbca16e8d347fae774e628c7274e6f
Peter Broadhurst <peter.broadhurst@kaleido.io>
2024-07-08 14:19:06 -0400

    Copy protobuf Makefile scaffolding to gable
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

---
+++ prototype/gable/tools/tools.go
@@ -0,0 +1,10 @@
+//go:build tools
+// +build tools
+
+// This package only exists to record tool dependencies, so they can be managed via go.mod
+package tools
+
+import (
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+)
