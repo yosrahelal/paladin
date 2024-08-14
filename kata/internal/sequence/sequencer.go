b26a66840380074fe0f9d581b690a53879c346a2
John Hosie <john.hosie@kaleido.io>
2024-08-14 06:18:59 +0100

    evaluate graph
    Signed-off-by: John Hosie <john.hosie@kaleido.io>

--- kata/internal/sequence/sequencer.go
+++ kata/internal/sequence/sequencer.go
@@ -20,8 +20,10 @@ import (
 	"context"
 
 	"github.com/google/uuid"
+	"github.com/hyperledger/firefly-common/pkg/i18n"
 	"github.com/hyperledger/firefly-common/pkg/log"
 	"github.com/kaleido-io/paladin/kata/internal/commsbus"
+	"github.com/kaleido-io/paladin/kata/internal/msgs"
 	"github.com/kaleido-io/paladin/kata/internal/statestore"
 	"github.com/kaleido-io/paladin/kata/internal/transactionstore"
 	pb "github.com/kaleido-io/paladin/kata/pkg/proto/sequence"
@@ -32,29 +34,75 @@ type EventSync interface {
 	Publish(event *commsbus.Event)
 }
 
+// an ordered list of transactions that are handed over to the dispatcher to be submitted to the base ledger in that order
+type Sequence []*transactionstore.Transaction
+
+type Dispatcher interface {
+	// Dispatcher is the component that takes responsibility for submitting the transactions in the sequence to the base ledger in the correct order
+	// most likely will be replaced with (or become an integration to) either the comms bus or some utility of the StageController framework
+	Dispatch(context.Context, []uuid.UUID) error
+}
+
 type Persistence interface {
 	// this is some temporary scaffolding around the core logic of the algorithm to allow us to make progress on the alogrithm and its functional tests
 	// before figuring how to integrate it into the main codebase and frameworks of Orchestrator, StageController, CommsBus etc...
 	// most likely will be replaced with some utility of the StageController or direct access to state store and transaction store etc..
 	GetStateByHash(context.Context, string) (statestore.State, error)
 	UpdateState(context.Context, statestore.State) error
+
 	GetTransactionByID(context.Context, uuid.UUID) (transactionstore.Transaction, error)
+	//return the transaction that has minted, or is assembled to mint the state with the given hash
+	GetMintingTransactionByStateHash(context.Context, string) (transactionstore.Transaction, error)
 }
 
 type Sequencer interface {
 	OnStateClaimEvent(ctx context.Context, event *pb.StateClaimEvent) error
+	OnTransactionAssembled(ctx context.Context, event *pb.TransactionAssembledEvent) error
+	OnTransactionEndorsed(ctx context.Context, event *pb.TransactionEndorsedEvent) error
 }
 
 type sequencer struct {
 	nodeID      uuid.UUID
 	persistence Persistence
 	commsBus    commsbus.CommsBus
+	resolver    ContentionResolver
+	dispatcher  Dispatcher
+	graph       Graph
 }
 
 func NewSequencer(eventSync EventSync, persistence Persistence) Sequencer {
 	return &sequencer{
 		persistence: persistence,
 	}
+}
+
+func (s *sequencer) evaluateGraph(ctx context.Context) error {
+	dispatchableTransactions, err := s.graph.GetDispatchableTransactions(ctx)
+	if err != nil {
+		log.L(ctx).Errorf("Error getting dispatchable transactions: %s", err)
+		return err
+	}
+	if len(dispatchableTransactions) == 0 {
+		return nil
+	}
+
+	transactionUUIDs := make([]uuid.UUID, len(dispatchableTransactions))
+	for i, txID := range dispatchableTransactions {
+		transactionUUID, err := uuid.Parse(txID)
+		if err != nil {
+			log.L(ctx).Errorf("failed to parse transaction ID as uuid: %s", txID)
+			return i18n.NewError(ctx, msgs.MsgSequencerInternalError, txID)
+		}
+		transactionUUIDs[i] = transactionUUID
+	}
+
+	err = s.dispatcher.Dispatch(ctx, transactionUUIDs)
+	if err != nil {
+		log.L(ctx).Errorf("Error dispatching transaction: %s", err)
+		return i18n.NewError(ctx, msgs.MsgSequencerInternalError, err)
+	}
+
+	return nil
 }
 
 func (s *sequencer) publishStateClaimLostEvent(ctx context.Context, stateHash, transactionID string) {
@@ -81,6 +129,39 @@ func (s *sequencer) sendReassembleMessage(ctx context.Context, transactionID str
 		log.L(ctx).Errorf("Error sending reassemble message: %s", err)
 	}
 }
+func (s *sequencer) OnTransactionAssembled(ctx context.Context, event *pb.TransactionAssembledEvent) error {
+	log.L(ctx).Infof("Received transaction assembled event: %s", event.String())
+
+	err := s.graph.AddTransaction(ctx, event.TransactionId, event.InputStateHash, event.OutputStateHash)
+	if err != nil {
+		log.L(ctx).Errorf("Error adding transaction to graph: %s", err)
+		return err
+	}
+	err = s.evaluateGraph(ctx)
+	if err != nil {
+		log.L(ctx).Errorf("Error evaluating graph: %s", err)
+		return err
+	}
+
+	return nil
+}
+func (s *sequencer) OnTransactionEndorsed(ctx context.Context, event *pb.TransactionEndorsedEvent) error {
+
+	log.L(ctx).Infof("Received transaction endorsed event: %s", event.String())
+
+	err := s.graph.RecordEndorsement(ctx, event.TransactionId)
+	if err != nil {
+		log.L(ctx).Errorf("Error recording endorsement: %s", err)
+		return err
+	}
+
+	s.evaluateGraph(ctx)
+	if err != nil {
+		log.L(ctx).Errorf("Error evaluating graph: %s", err)
+		return err
+	}
+	return nil
+}
 
 func (s *sequencer) OnStateClaimEvent(ctx context.Context, event *pb.StateClaimEvent) error {
 	log.L(ctx).Infof("Received state claim event: %s", event.String())
@@ -91,7 +172,11 @@ func (s *sequencer) OnStateClaimEvent(ctx context.Context, event *pb.StateClaimE
 	}
 	if state.ClaimedBy != nil {
 		//we have a contention
-		resolvedClaimer, err := ContentionResolver(state.Hash.String(), state.ClaimedBy.String(), event.TransactionId)
+		log.L(ctx).Debug("Contention")
+		// if this is the only state being claimed by both transactions, then we can resolve the contention immediately
+		// if either of the transactions is claiming more than one state, then the is no guarantee that we can predict the final outcome
+		// so all we can do is add both transactions to a sequence and let that sequencer
+		resolvedClaimer, err := s.resolver.Resolve(state.Hash.String(), state.ClaimedBy.String(), event.TransactionId)
 		if err != nil {
 			log.L(ctx).Errorf("Error resolving contention: %s", err)
 			return err
@@ -106,7 +191,8 @@ func (s *sequencer) OnStateClaimEvent(ctx context.Context, event *pb.StateClaimE
 			if currentClaimer.NodeID == s.nodeID {
 
 				// if the loser is assembled by the current node, then send a message to the assembler to reassemble
-				// TODO - not sure this is exactly how the orchestrator expects us to deal with this.  Should we be sending a message to the orchestrator to update the transaction state and let the assemble stage notice that on its next cycle?
+				// TODO - not sure this is exactly how the orchestrator expects us to deal with this.
+				//Should we be sending a message to the orchestrator to update the transaction state and let the assemble stage notice that on its next cycle?
 
 				//Before sending that message, we need to ensure that the DB is updated with the new claimer otherwise the assembler will attempt to claim this new state again
 				resolvedClaimerUUID, err := uuid.Parse(resolvedClaimer)
