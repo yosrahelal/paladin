7f26ea6f39c66d2cdd0dff7643270ed5b216906c
Andrew Richardson <andrew.richardson@kaleido.io>
2024-08-14 18:58:32 -0400

    Add arg for Docker compose file
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- buildSrc/src/main/groovy/DockerCompose.groovy
+++ buildSrc/src/main/groovy/DockerCompose.groovy
@@ -1,30 +1,46 @@
+import org.gradle.api.GradleException
 import org.gradle.api.tasks.Exec
 import org.gradle.process.ExecResult
 
 class DockerCompose extends Exec {
+
+    private String _composeFile = 'docker-compose.yml'
+
     DockerCompose() {
-        def String composeCommand
-        def dockerComposeV2Check = "docker compose version".execute().text
-        if (dockerComposeV2Check.contains("Docker Compose")){
-            executable 'docker'
-            args 'compose'
-            composeCommand = 'docker compose'
-        } else {
-            executable 'docker-compose'
-            composeCommand = 'docker-compose'
-        }
-        args '-f'
-        args 'docker-compose-test.yml'
-
         ignoreExitValue true
-
-        doLast {
-            def ExecResult execResult = getExecutionResult().get();
-            if(execResult.getExitValue() != 0) {
-                println "${composeCommand} -f ${getProject().projectDir}/docker-compose-test.yml logs".execute().waitForProcessOutput(System.err,System.err)
-                throw new Exception("Docker compose failed");
-            }
-        }
-
     }
+
+    void composeFile(String f) {
+        this._composeFile = f
+    }
+
+    private String findExecutable() {
+        String composeCommand
+        String dockerComposeV2Check = 'docker compose version'.execute().text
+        if (dockerComposeV2Check.contains('Docker Compose')) {
+            executable = 'docker'
+            args = ['compose', *args]
+            return 'docker compose'
+        } else {
+            executable = 'docker-compose'
+            return 'docker-compose'
+        }
+    }
+
+    @Override
+    protected void exec() {
+        args = ['-f', this._composeFile, *args]
+        String composeCommand = findExecutable()
+
+        super.exec()
+
+        ExecResult execResult = executionResult.get()
+        if (execResult.exitValue != 0) {
+            println "${composeCommand} -f ${project.projectDir}/docker-compose-test.yml logs"
+                .execute()
+                .waitForProcessOutput(System.err, System.err)
+            throw new GradleException('Docker compose failed')
+        }
+    }
+
 }
