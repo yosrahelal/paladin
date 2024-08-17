7606b226bda805e32318d4d8788f377cb7199b68
Andrew Richardson <andrew.richardson@kaleido.io>
2024-08-16 20:33:35 -0400

    Add projectName option to DockerCompose
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- buildSrc/src/main/groovy/DockerCompose.groovy
+++ buildSrc/src/main/groovy/DockerCompose.groovy
@@ -4,6 +4,7 @@ import org.gradle.api.DefaultTask
 class DockerCompose extends DefaultTask {
 
     private List<String> _composeFiles = []
+    private List<String> _projectName = []
     private List<String> _args = []
 
     DockerCompose() {
@@ -16,18 +17,24 @@ class DockerCompose extends DefaultTask {
         _composeFiles += ['-f', f]
     }
 
+    void projectName(String p) {
+        _projectName = ['-p', p]
+    }
+
     void args(Object... args) {
         _args += [*args]
     }
 
-    void dumpLogs(String service = "") {
-        if (service == "") {
-            println "Dumping Docker logs"
+    void dumpLogs(String service = '') {
+        if (service == '') {
+            println 'Dumping Docker logs'
         } else {
             println "Dumping Docker logs for ${service}"
         }
-        "${dockerCommand().join(' ')} logs ${service}"
-            .execute().waitForProcessOutput(System.err, System.err)
+        new ProcessBuilder().command(*dockerCommand(), 'logs', service)
+            .directory(project.projectDir)
+            .start()
+            .waitForProcessOutput(System.err, System.err)
     }
 
     private List<String> dockerCommand() {
@@ -35,10 +42,9 @@ class DockerCompose extends DefaultTask {
             _composeFiles = ['-f', 'docker-compose.yml']
         }
         String dockerComposeV2Check = 'docker compose version'.execute().text
-        if (dockerComposeV2Check.contains('Docker Compose')) {
-            return ['docker', 'compose', *_composeFiles]
-        }
-        return ['docker-compose', *_composeFiles]
+        return dockerComposeV2Check.contains('Docker Compose')
+            ? ['docker', 'compose', *_composeFiles, *_projectName]
+            : ['docker-compose', *_composeFiles, *_projectName]
     }
 
     protected void exec() {
