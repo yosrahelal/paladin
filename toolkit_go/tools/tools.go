e400b5fcfdb260e5fd80226dc7ab256e13a692fb
Peter Broadhurst <peter.broadhurst@kaleido.io>
2024-08-17 19:33:29 -0400

    Copy build over to toolkit go
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

---
+++ toolkit_go/tools/tools.go
@@ -0,0 +1,12 @@
+//go:build tools
+// +build tools
+
+// This package only exists to record tool dependencies, so they can be managed via go.mod
+package tools
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/vektra/mockery/v2"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+)
