f177417ff814ce8048a4135920ed70c3850da4b1
John Hosie <john.hosie@kaleido.io>
2024-08-31 17:49:18 +0100

    graph scenario 2
    Signed-off-by: John Hosie <john.hosie@kaleido.io>

--- kata/internal/engine/sequencer/graph_test.go
+++ kata/internal/engine/sequencer/graph_test.go
@@ -133,6 +133,15 @@ func TestGetDispatchableTransactions(t *testing.T) {
 }
 
 func TestScenario1(t *testing.T) {
+	// 5 transactions, 0,1,2,3,4
+	// 3 depends on 0 and 1
+	// 4 depends on 1 and 2
+	// 0, 1, 3 and 4 are endorsed
+	// only 0, 1 and 3 should be dispatchable (because 4 depends on 2 which is not endorsed)
+
+	// 3 is sequenced after both 0 and 1 because it depends on both
+	// order of 0 and 1 does not matter because they are not dependent on each other
+
 	// build the matrix by adding transactions
 	ctx := context.Background()
 	testGraph := NewGraph()
@@ -194,5 +203,85 @@ func TestScenario1(t *testing.T) {
 	dispatchableTransactions, err = testGraph.GetDispatchableTransactions(ctx)
 	assert.NoError(t, err)
 	assert.Len(t, dispatchableTransactions, 0)
+
+}
+
+func TestScenario2(t *testing.T) {
+	// Test the breadth first search
+	// 6 transactions, 0,1,2,3,4, 5
+	// 4 depends on 2 which depends on 0
+	// 5 depends on 3 which depends on 1
+	// all are endorsed
+
+	// ordering should respect the dependency relationships ( 4 before 2, 2 before 0, 5 before 3, 3 before 1)
+	// ordering should also bias towards transactions closer to the independent edge of the graph (1 before 2 and 0 before 3 even though they are not directly dependent)
+
+	// build the matrix by adding transactions
+	ctx := context.Background()
+	testGraph := NewGraph()
+	err := testGraph.AddTransaction(ctx, "tx0", []string{}, []string{"S0"})
+	require.NoError(t, err)
+
+	err = testGraph.AddTransaction(ctx, "tx1", []string{}, []string{"S1"})
+	require.NoError(t, err)
+
+	err = testGraph.AddTransaction(ctx, "tx2", []string{"S0"}, []string{"S2"})
+	require.NoError(t, err)
+
+	err = testGraph.AddTransaction(ctx, "tx3", []string{"S1"}, []string{"S3"})
+	require.NoError(t, err)
+
+	err = testGraph.AddTransaction(ctx, "tx4", []string{"S2"}, []string{"S4"})
+	require.NoError(t, err)
+
+	err = testGraph.AddTransaction(ctx, "tx5", []string{"S3"}, []string{"S5"})
+	require.NoError(t, err)
+
+	err = testGraph.RecordEndorsement(ctx, "tx0")
+	require.NoError(t, err)
+
+	err = testGraph.RecordEndorsement(ctx, "tx1")
+	require.NoError(t, err)
+
+	err = testGraph.RecordEndorsement(ctx, "tx2")
+	require.NoError(t, err)
+
+	err = testGraph.RecordEndorsement(ctx, "tx3")
+	require.NoError(t, err)
+
+	err = testGraph.RecordEndorsement(ctx, "tx4")
+	require.NoError(t, err)
+
+	err = testGraph.RecordEndorsement(ctx, "tx5")
+	require.NoError(t, err)
+
+	dispatchableTransactions, err := testGraph.GetDispatchableTransactions(ctx)
+	assert.NoError(t, err)
+	require.Len(t, dispatchableTransactions, 6)
+
+	//make sure they come out in the expected order
+	// the absolute order does not matter so long as dependencies come before dependants.
+	// so we expect either 0,1,3 or 1,0,3.
+	isBefore := func(tx1, tx2 string) bool {
+		foundTx1 := false
+		for _, tx := range dispatchableTransactions {
+			switch tx {
+			case tx1:
+				foundTx1 = true
+			case tx2:
+				return foundTx1
+			}
+		}
+		assert.Failf(t, "%s not found", tx2)
+		return false
+	}
+	assert.True(t, isBefore("tx0", "tx2"))
+	assert.True(t, isBefore("tx0", "tx3"))
+	assert.True(t, isBefore("tx1", "tx2"))
+	assert.True(t, isBefore("tx1", "tx3"))
+	assert.True(t, isBefore("tx2", "tx4"))
+	assert.True(t, isBefore("tx2", "tx5"))
+	assert.True(t, isBefore("tx3", "tx4"))
+	assert.True(t, isBefore("tx3", "tx5"))
 
 }
