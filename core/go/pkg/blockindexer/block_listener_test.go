10298a201e21a1039e6f0557a637d4fad49df703
dwertent <david.wertenteil@kaleido.io>
2024-09-18 13:42:23 -0400

    chore: Update blockListener to reset polling interval after shoulder tap
    Signed-off-by: dwertent <david.wertenteil@kaleido.io>

--- core/go/pkg/blockindexer/block_listener_test.go
+++ core/go/pkg/blockindexer/block_listener_test.go
@@ -76,8 +76,15 @@ func newTestBlockListenerConf(t *testing.T, ctx context.Context, config *Config)
 }
 
 func TestBlockListenerStartGettingHighestBlockRetry(t *testing.T) {
-
 	ctx, bl, mRPC, done := newTestBlockListener(t)
+
+	// Mock eth_newBlockFilter call (happens on initialization)
+	mRPC.On("CallRPC", mock.Anything, mock.Anything, "eth_newBlockFilter").
+		Return(nil).Once()
+
+	// Mock eth_getFilterChanges call (this is called after eth_newBlockFilter)
+	mRPC.On("CallRPC", mock.Anything, mock.Anything, "eth_getFilterChanges", "").
+		Return(nil).Once()
 
 	mRPC.On("CallRPC", mock.Anything, mock.Anything, "eth_blockNumber").
 		Return(&rpcbackend.RPCError{Message: "pop"}).Once()
@@ -94,7 +101,6 @@ func TestBlockListenerStartGettingHighestBlockRetry(t *testing.T) {
 	done() // Stop immediately in this case, while we're in the polling interval
 
 	<-bl.listenLoopDone
-
 }
 
 func TestBlockListenerStartGettingHighestBlockFailBeforeStop(t *testing.T) {
@@ -1430,6 +1436,7 @@ func TestBlockListenerClosedBeforeEstablishingBlockHeight(t *testing.T) {
 		Return(&rpcbackend.RPCError{Message: "pop"}).Once()
 
 	bl.listenLoopDone = make(chan struct{})
-	bl.listenLoop()
+	listenerInitiated := make(chan struct{})
+	bl.listenLoop(&listenerInitiated)
 
 }
