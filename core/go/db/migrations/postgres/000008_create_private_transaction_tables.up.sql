20031f20b93c82ddc99179c9b75afa7123f099bd
John Hosie <john.hosie@kaleido.io>
2024-09-18 19:44:12 +0100

    dispatches table
    Signed-off-by: John Hosie <john.hosie@kaleido.io>

---
+++ core/go/db/migrations/postgres/000008_create_private_transaction_tables.up.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+CREATE TABLE dispatches (
+    "public_transaction_id"       UUID    NOT NULL,
+    "private_transaction_id"      TEXT    NOT NULL,
+    "id"                          TEXT    NOT NULL,
+    PRIMARY KEY ("id")
+);
+
+CREATE UNIQUE INDEX dispatches_public_private ON dispatches("public_transaction_id","private_transaction_id");
+
+COMMIT;
+
---
+++ core/go/db/migrations/sqlite/000008_create_private_transaction_tables.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE dispatches (
+    "public_transaction_id"       UUID    NOT NULL,
+    "private_transaction_id"      TEXT    NOT NULL,
+    "id"                          TEXT    NOT NULL,
+    PRIMARY KEY ("id")
+);
+
+CREATE UNIQUE INDEX dispatches_public_private ON dispatches("public_transaction_id","private_transaction_id");
