a085672b0851447805eb5946b5e3e1395763dca1
Andrew Richardson <andrew.richardson@kaleido.io>
2024-09-30 23:05:19 -0400

    Solidity unit test for Pente external call
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- solidity/test/domains/pente/PentePrivacyGroup.ts
+++ solidity/test/domains/pente/PentePrivacyGroup.ts
@@ -1,5 +1,11 @@
 import { expect } from "chai";
-import { AbiCoder, concat, Signer, TypedDataEncoder } from "ethers";
+import {
+  AbiCoder,
+  AddressLike,
+  concat,
+  Signer,
+  TypedDataEncoder,
+} from "ethers";
 import hre from "hardhat";
 import { PentePrivacyGroup } from "../../../typechain-types";
 const abiCoder = AbiCoder.defaultAbiCoder();
@@ -9,7 +15,7 @@ enum PenteConfigID {
 }
 
 interface ExternalCall {
-  contractAddress: string;
+  contractAddress: AddressLike;
   encodedCall: string;
 }
 
@@ -295,5 +301,52 @@ describe("PentePrivacyGroup", function () {
     )
       .to.revertedWithCustomError(privacyGroup, "PenteOutputAlreadyUnspent")
       .withArgs(stateSet1[0]);
+  });
+
+  it("successful transition with external call", async function () {
+    const { privacyGroup, endorsers } = await pentePrivacyGroupSetup();
+
+    const [deployer, other] = await hre.ethers.getSigners();
+    const ERC20 = await hre.ethers.getContractFactory("ERC20Simple");
+    const erc20 = await ERC20.deploy("COIN", "COIN");
+    await erc20.mint(deployer, 100);
+    await erc20.approve(await privacyGroup.getAddress(), 100);
+
+    const stateSet1 = [randBytes32(), randBytes32(), randBytes32()];
+    const externalCalls = [
+      {
+        contractAddress: await erc20.getAddress(),
+        encodedCall: erc20.interface.encodeFunctionData("transferFrom", [
+          deployer.address,
+          other.address,
+          100,
+        ]),
+      },
+    ];
+
+    const { signatures: endorsements1 } = await newTransitionEIP712(
+      privacyGroup,
+      [],
+      [],
+      stateSet1,
+      externalCalls,
+      endorsers
+    );
+    const tx1ID = randBytes32();
+
+    await expect(
+      privacyGroup.transition(
+        tx1ID,
+        [],
+        [],
+        stateSet1,
+        externalCalls,
+        endorsements1
+      )
+    )
+      .to.emit(privacyGroup, "UTXOTransfer")
+      .withArgs(tx1ID, [], stateSet1, "0x")
+      .and.to.emit(erc20, "Transfer")
+      .withArgs(deployer, other, 100);
   });
 });
