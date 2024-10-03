d496c839c2a17a8be208599fb68349905378618e
Jim Zhang <jim.zhang@kaleido.io>
2024-10-03 08:07:50 -0400

    Fix duplicate imports due to merge
    Signed-off-by: Jim Zhang <jim.zhang@kaleido.io>

--- domains/zeto/pkg/zetosigner/snark_prover_test.go
+++ domains/zeto/pkg/zetosigner/snark_prover_test.go
@@ -31,7 +31,6 @@ import (
 	"github.com/kaleido-io/paladin/config/pkg/confutil"
 	"github.com/kaleido-io/paladin/domains/zeto/pkg/constants"
 	pb "github.com/kaleido-io/paladin/domains/zeto/pkg/proto"
-	"github.com/kaleido-io/paladin/toolkit/pkg/confutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/proto"
