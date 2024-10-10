e2996ada36042e6851d65198fe625d47d9d0d768
John Hosie <john.hosie@kaleido.io>
2024-10-10 13:01:37 +0100

    refactor
    Signed-off-by: John Hosie <john.hosie@kaleido.io>

--- core/go/internal/statedistribution/state_distributer.go
+++ core/go/internal/statedistribution/state_distributer.go
@@ -17,7 +17,6 @@ package statedistribution
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/kaleido-io/paladin/config/pkg/pldconf"
@@ -25,14 +24,11 @@ import (
 	"github.com/kaleido-io/paladin/core/internal/privatetxnmgr/privatetxnstore"
 	"github.com/kaleido-io/paladin/core/internal/privatetxnmgr/ptmgrtypes"
 	"github.com/kaleido-io/paladin/core/pkg/persistence"
-	pb "github.com/kaleido-io/paladin/core/pkg/proto/sequence"
 	"github.com/kaleido-io/paladin/toolkit/pkg/log"
 	"github.com/kaleido-io/paladin/toolkit/pkg/tktypes"
-	"google.golang.org/protobuf/proto"
 )
 
 const RETRY_TIMEOUT = 5 * time.Second
-const STATE_DISTRIBUTER_DESTINATION = "state-distributer"
 
 func NewStateDistributer(ctx context.Context, nodeID string, transportManager components.TransportManager, stateManager components.StateManager, persistence persistence.Persistence, conf *pldconf.StateDistributerConfig) ptmgrtypes.StateDistributer {
 	sd := &stateDistributer{
@@ -153,161 +149,4 @@ func (sd *stateDistributer) Start(ctx context.Context) error {
 
 func (sd *stateDistributer) Stop(ctx context.Context) {
 	close(sd.stopChan)
-}
-
-func (sd *stateDistributer) AcknowledgeState(ctx context.Context, stateDistributionID string) {
-	log.L(ctx).Debugf("stateDistributer:AcknowledgeState %s ", stateDistributionID)
-	sd.acknowledgedChan <- stateDistributionID
-}
-
-func (sd *stateDistributer) DistributeStates(ctx context.Context, stateDistributions []*ptmgrtypes.StateDistribution) {
-	log.L(ctx).Debugf("stateDistributer:DistributeStates %d state distributions", len(stateDistributions))
-	for _, stateDistribution := range stateDistributions {
-		sd.inputChan <- stateDistribution
-	}
-}
-
-func (sd *stateDistributer) sendState(ctx context.Context, stateDistribution *ptmgrtypes.StateDistribution) {
-	log.L(ctx).Debugf("stateDistributer:sendState %s %s %s %s %s %s", stateDistribution.Domain, stateDistribution.ContractAddress, stateDistribution.SchemaID, stateDistribution.StateID, stateDistribution.IdentityLocator, stateDistribution.ID)
-
-	stateProducedEvent := &pb.StateProducedEvent{
-		DomainName:      stateDistribution.Domain,
-		ContractAddress: stateDistribution.ContractAddress,
-		SchemaId:        stateDistribution.SchemaID,
-		StateId:         stateDistribution.StateID,
-		StateDataJson:   stateDistribution.StateDataJson,
-		Party:           stateDistribution.IdentityLocator,
-		DistributionId:  stateDistribution.ID,
-	}
-	stateProducedEventBytes, err := proto.Marshal(stateProducedEvent)
-	if err != nil {
-		log.L(ctx).Errorf("Error marshalling delegate transaction message: %s", err)
-		return
-	}
-
-	targetNode, err := tktypes.PrivateIdentityLocator(stateDistribution.IdentityLocator).Node(ctx, false)
-	if err != nil {
-		log.L(ctx).Errorf("Error getting node for party %s", stateDistribution.IdentityLocator)
-		return
-	}
-
-	err = sd.transportManager.Send(ctx, &components.TransportMessage{
-		MessageType: "StateProducedEvent",
-		Payload:     stateProducedEventBytes,
-		Destination: tktypes.PrivateIdentityLocator(fmt.Sprintf("%s@%s", STATE_DISTRIBUTER_DESTINATION, targetNode)),
-		ReplyTo:     tktypes.PrivateIdentityLocator(fmt.Sprintf("%s@%s", STATE_DISTRIBUTER_DESTINATION, sd.nodeID)),
-	})
-	if err != nil {
-		log.L(ctx).Errorf("Error sending state produced event: %s", err)
-		return
-	}
-
-	go func() {
-		time.Sleep(RETRY_TIMEOUT)
-		sd.retryChan <- stateDistribution.ID
-	}()
-
-}
-
-func (sd *stateDistributer) sendStateAcknowledgement(ctx context.Context, domainName string, contractAddress string, stateId string, receivingParty string, distributingNode string, distributionID string) error {
-	log.L(ctx).Debugf("stateDistributer:sendStateAcknowledgement %s %s %s %s %s %s", domainName, contractAddress, stateId, receivingParty, distributingNode, distributionID)
-	stateAcknowledgedEvent := &pb.StateAcknowledgedEvent{
-		DomainName:      domainName,
-		ContractAddress: contractAddress,
-		StateId:         stateId,
-		Party:           receivingParty,
-		DistributionId:  distributionID,
-	}
-	stateAcknowledgedEventBytes, err := proto.Marshal(stateAcknowledgedEvent)
-	if err != nil {
-		log.L(ctx).Errorf("Error marshalling state acknowledgment event: %s", err)
-		return err
-	}
-
-	err = sd.transportManager.Send(ctx, &components.TransportMessage{
-		MessageType: "StateAcknowledgedEvent",
-		Payload:     stateAcknowledgedEventBytes,
-		Destination: tktypes.PrivateIdentityLocator(fmt.Sprintf("%s@%s", STATE_DISTRIBUTER_DESTINATION, distributingNode)),
-		ReplyTo:     tktypes.PrivateIdentityLocator(fmt.Sprintf("%s@%s", STATE_DISTRIBUTER_DESTINATION, sd.nodeID)),
-	})
-	if err != nil {
-		log.L(ctx).Errorf("Error sending state produced event: %s", err)
-		return err
-	}
-
-	return nil
-}
-
-func (sd *stateDistributer) Destination() string {
-	return STATE_DISTRIBUTER_DESTINATION
-}
-
-func (sd *stateDistributer) ReceiveTransportMessage(ctx context.Context, message *components.TransportMessage) {
-	log.L(ctx).Debugf("stateDistributer:ReceiveTransportMessage")
-	messagePayload := message.Payload
-	replyToDestination := message.ReplyTo
-
-	switch message.MessageType {
-	case "StateProducedEvent":
-		//Not sure this message really needs to come into the private tx manager, just to be forwarded to the state store.
-		distributingNode, err := replyToDestination.Node(ctx, false)
-		if err != nil {
-			log.L(ctx).Errorf("Error getting node for party %s", replyToDestination)
-			return
-		}
-		go sd.handleStateProducedEvent(ctx, messagePayload, distributingNode)
-	case "StateAcknowledgedEvent":
-		go sd.handleStateAcknowledgedEvent(ctx, message.Payload)
-	default:
-		log.L(ctx).Errorf("Unknown message type: %s", message.MessageType)
-	}
-}
-
-func (sd *stateDistributer) handleStateProducedEvent(ctx context.Context, messagePayload []byte, distributingNode string) {
-	log.L(ctx).Debugf("stateDistributer:handleStateProducedEvent")
-	stateProducedEvent := &pb.StateProducedEvent{}
-	err := proto.Unmarshal(messagePayload, stateProducedEvent)
-	if err != nil {
-		log.L(ctx).Errorf("Failed to unmarshal StateProducedEvent: %s", err)
-		return
-	}
-
-	err = sd.receivedStateWriter.QueueAndWait(ctx, stateProducedEvent.DomainName, *tktypes.MustEthAddress(stateProducedEvent.ContractAddress), tktypes.MustParseBytes32(stateProducedEvent.SchemaId), tktypes.RawJSON(stateProducedEvent.StateDataJson))
-	if err != nil {
-		log.L(ctx).Errorf("Error writing state: %s", err)
-		//don't send the acknowledgement, with a bit of luck, the sender will retry and we will get it next time
-		return
-	}
-
-	// No error means either this is the first time we have received this state or we already have it an onConflict ignore means we idempotently accept it
-	// If the latter, then the sender probably didn't get our previous acknowledgement so either way, we send an acknowledgement
-
-	err = sd.sendStateAcknowledgement(
-		ctx,
-		stateProducedEvent.DomainName,
-		stateProducedEvent.ContractAddress,
-		stateProducedEvent.StateId,
-		stateProducedEvent.Party,
-		distributingNode,
-		stateProducedEvent.DistributionId)
-	if err != nil {
-		log.L(ctx).Errorf("Error sending state acknowledgement: %s", err)
-		//not much more we can do here.  The sender will inevitably retry and we will hopefully send the ack next time
-	}
-}
-
-func (sd *stateDistributer) handleStateAcknowledgedEvent(ctx context.Context, messagePayload []byte) {
-	log.L(ctx).Debugf("stateDistributer:handleStateAcknowledgedEvent")
-	stateAcknowledgedEvent := &pb.StateAcknowledgedEvent{}
-	err := proto.Unmarshal(messagePayload, stateAcknowledgedEvent)
-	if err != nil {
-		log.L(ctx).Errorf("Failed to unmarshal StateAcknowledgedEvent: %s", err)
-		return
-	}
-	sd.acknowledgementWriter.Queue(ctx, stateAcknowledgedEvent.DistributionId)
-	// no need to wait for the flush to complete, we can just stop the in memory loop from retrying
-	// worst case scenario, we crash before this is written to the DB, we do some redundant retries after a restart
-	// but waiting for the flush here is not going to prevent that
-	sd.acknowledgedChan <- stateAcknowledgedEvent.DistributionId
-
 }
---
+++ core/go/internal/statedistribution/state_receiver.go
@@ -0,0 +1,61 @@
+/*
+ * Copyright © 2024 Kaleido, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package statedistribution
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/kaleido-io/paladin/core/internal/components"
+	pb "github.com/kaleido-io/paladin/core/pkg/proto/sequence"
+	"github.com/kaleido-io/paladin/toolkit/pkg/log"
+	"github.com/kaleido-io/paladin/toolkit/pkg/tktypes"
+	"google.golang.org/protobuf/proto"
+)
+
+func (sd *stateDistributer) AcknowledgeState(ctx context.Context, stateDistributionID string) {
+	log.L(ctx).Debugf("stateDistributer:AcknowledgeState %s ", stateDistributionID)
+	sd.acknowledgedChan <- stateDistributionID
+}
+
+func (sd *stateDistributer) sendStateAcknowledgement(ctx context.Context, domainName string, contractAddress string, stateId string, receivingParty string, distributingNode string, distributionID string) error {
+	log.L(ctx).Debugf("stateDistributer:sendStateAcknowledgement %s %s %s %s %s %s", domainName, contractAddress, stateId, receivingParty, distributingNode, distributionID)
+	stateAcknowledgedEvent := &pb.StateAcknowledgedEvent{
+		DomainName:      domainName,
+		ContractAddress: contractAddress,
+		StateId:         stateId,
+		Party:           receivingParty,
+		DistributionId:  distributionID,
+	}
+	stateAcknowledgedEventBytes, err := proto.Marshal(stateAcknowledgedEvent)
+	if err != nil {
+		log.L(ctx).Errorf("Error marshalling state acknowledgment event: %s", err)
+		return err
+	}
+
+	err = sd.transportManager.Send(ctx, &components.TransportMessage{
+		MessageType: "StateAcknowledgedEvent",
+		Payload:     stateAcknowledgedEventBytes,
+		Destination: tktypes.PrivateIdentityLocator(fmt.Sprintf("%s@%s", STATE_DISTRIBUTER_DESTINATION, distributingNode)),
+		ReplyTo:     tktypes.PrivateIdentityLocator(fmt.Sprintf("%s@%s", STATE_DISTRIBUTER_DESTINATION, sd.nodeID)),
+	})
+	if err != nil {
+		log.L(ctx).Errorf("Error sending state produced event: %s", err)
+		return err
+	}
+
+	return nil
+}
