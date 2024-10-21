8765d0a0fd37e9d172231fd31b32054889bcbfed
Peter Broadhurst <peter.broadhurst@kaleido.io>
2024-10-21 15:57:35 -0400

    Build fixes
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

---
+++ toolkit/go/pkg/pldapi/enum_test.go
@@ -0,0 +1,31 @@
+// Copyright © 2024 Kaleido, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pldapi
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEnums(t *testing.T) {
+	assert.NotEmpty(t, EthTransactionResult("").Enum().Options())
+	assert.NotEmpty(t, TransactionType("").Enum().Options())
+	assert.NotEmpty(t, ActiveFilter("").Enum().Options())
+	assert.NotEmpty(t, SchemaType("").Enum().Options())
+	assert.NotEmpty(t, StateLockType("").Enum().Options())
+}
