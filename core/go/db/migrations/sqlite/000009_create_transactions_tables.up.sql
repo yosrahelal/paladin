ebdcb9ced25e297893749969d0f8fc42d347363a
Peter Broadhurst <peter.broadhurst@kaleido.io>
2024-10-30 18:55:43 -0400

    Only require public TX to exist for binding
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

--- core/go/db/migrations/sqlite/000009_create_transactions_tables.up.sql
+++ core/go/db/migrations/sqlite/000009_create_transactions_tables.up.sql
@@ -43,7 +43,6 @@ CREATE TABLE public_txn_bindings (
   "transaction"               UUID            NOT NULL,
   "tx_type"                   TEXT            NOT NULL,
   PRIMARY KEY ("signer_nonce"), -- a binding is not mandatory for a public TXN, but it is singular (see #210)
-  FOREIGN KEY ("transaction") REFERENCES transactions ("id") ON DELETE CASCADE,
   FOREIGN KEY ("signer_nonce") REFERENCES public_txns ("signer_nonce") ON DELETE CASCADE
 );
 CREATE INDEX public_txn_bindings_transaction ON public_txn_bindings("transaction");
