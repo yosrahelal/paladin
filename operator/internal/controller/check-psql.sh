2ac4f71fe244f08701022c58a2db154fdb6ac6d4
hfuss <hayden.fuss@kaleido.io>
2024-11-07 13:01:21 -0500

    better shortNames; simplifying make target; avoiding too aggressive of sql checking
    Signed-off-by: hfuss <hayden.fuss@kaleido.io>

--- operator/internal/controller/check-psql.sh
+++ operator/internal/controller/check-psql.sh
@@ -2,8 +2,8 @@
 
 count=0
 
-until [ $count -ge 3 ]; do
+until [ $count -ge 5 ]; do
   psql -U postgres -c 'SELECT 1' && count=$((count + 1))
   echo "Waiting for postgres to be ready..."
-  sleep 2
+  sleep 1
 done
