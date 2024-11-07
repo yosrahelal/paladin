d60e8bb12679c5d42c70b46d1cb5bc3b857337eb
hfuss <hayden.fuss@kaleido.io>
2024-11-07 12:42:27 -0500

    cooking w/ gas
    Signed-off-by: hfuss <hayden.fuss@kaleido.io>

--- operator/internal/controller/inflight.go
+++ operator/internal/controller/inflight.go
@@ -17,7 +17,7 @@ func NewInFlight() *InFlight {
 	return &InFlight{
 		mux:                &sync.RWMutex{},
 		inFlight:           make(map[string]time.Time),
-		changeWaitDuration: 30 * time.Second,
+		changeWaitDuration: 20 * time.Second,
 	}
 }
 
