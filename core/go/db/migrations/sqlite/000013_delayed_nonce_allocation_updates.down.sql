9ed1d273bd9a59ac7c1ef13e4cdba8b3c9a15769
Peter Broadhurst <peter.broadhurst@kaleido.io>
2024-11-11 18:58:25 +0700

    SQLite migration to create new DB (no data migration)
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

--- core/go/db/migrations/sqlite/000013_delayed_nonce_allocation_updates.down.sql
+++ core/go/db/migrations/sqlite/000013_delayed_nonce_allocation_updates.down.sql
@@ -1,5 +1,1 @@
-BEGIN;
-
 -- TODO (or at least decide if TODO): down migration is provided to reinstate the signer_nonce column.
-
-COMMIT;
---
+++ core/go/db/migrations/sqlite/000013_delayed_nonce_allocation_updates.up.sql
@@ -0,0 +1,48 @@
+-- For SQLite we do not propagate public Tx data in created before migration 13
+DROP TABLE IF EXISTS public_txn_bindings;
+DROP TABLE IF EXISTS public_submissions;
+DROP TABLE IF EXISTS public_completions;
+DROP TABLE IF EXISTS public_txns;
+
+CREATE TABLE public_txns (
+  "pub_txn_id"                INTEGER         PRIMARY KEY AUTOINCREMENT,
+  "from"                      TEXT            NOT NULL,
+  "nonce"                     BIGINT,
+  "created"                   BIGINT          NOT NULL,
+  "to"                        TEXT,
+  "gas"                       BIGINT          NOT NULL,
+  "fixed_gas_pricing"         TEXT,
+  "value"                     TEXT,
+  "data"                      TEXT,
+  "suspended"                 BOOLEAN         NOT NULL
+);
+CREATE UNIQUE INDEX public_txns_from_nonce ON public_txns("from", "nonce");
+
+CREATE TABLE public_submissions (
+  "tx_hash"                   TEXT            NOT NULL,
+  "pub_txn_id"                BIGINT          NOT NULL,
+  "created"                   BIGINT          NOT NULL,
+  "gas_pricing"               TEXT,
+  PRIMARY KEY("tx_hash"),
+  FOREIGN KEY ("pub_txn_id") REFERENCES public_txns ("pub_txn_id") ON DELETE CASCADE
+);
+CREATE INDEX public_submissions_pub_txn_id on public_submissions("pub_txn_id");
+
+CREATE TABLE public_completions (
+  "pub_txn_id"                INTEGER         NOT NULL,
+  "created"                   BIGINT          NOT NULL,
+  "tx_hash"                   TEXT            NOT NULL,
+  "success"                   BOOLEAN         NOT NULL,
+  "revert_data"               TEXT,
+  FOREIGN KEY ("pub_txn_id") REFERENCES public_txns ("pub_txn_id") ON DELETE CASCADE,
+  PRIMARY KEY("pub_txn_id")
+);
+
+CREATE TABLE public_txn_bindings (
+  "pub_txn_id"                INTEGER         NOT NULL,
+  "transaction"               UUID            NOT NULL,
+  "tx_type"                   VARCHAR         NOT NULL,
+  PRIMARY KEY ("pub_txn_id"),
+  FOREIGN KEY ("pub_txn_id") REFERENCES public_txns ("pub_txn_id") ON DELETE CASCADE
+);
+CREATE INDEX public_txn_bindings_transaction ON public_txn_bindings("transaction");
