1818616b930c4e75e40eb3bd527cf90ac7c055a4
Jim Zhang <jim.zhang@kaleido.io>
2024-12-17 14:23:06 -0500

    add lockProof() to the zeto private solidity
    Signed-off-by: Jim Zhang <jim.zhang@kaleido.io>

--- solidity/contracts/domains/interfaces/IZeto.sol
+++ solidity/contracts/domains/interfaces/IZeto.sol
@@ -14,6 +14,7 @@ interface IZeto {
 
     function mint(TransferParam[] memory mints) external;
     function transfer(TransferParam[] memory transfers) external;
+    function lockProof(address delegate, bytes memory call) external;
     function deposit(uint256 amount) external;
     function withdraw(uint256 amount) external;
     function setERC20(address erc20) external;
