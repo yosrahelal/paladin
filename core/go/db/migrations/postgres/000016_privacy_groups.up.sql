825cd3286643ed8a5d9f764f4ab387a213d5e819
Peter Broadhurst <peter.broadhurst@kaleido.io>
2025-01-26 11:43:35 -0500

    DB design for privacy groups
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

---
+++ core/go/db/migrations/postgres/000016_privacy_groups.up.sql
@@ -0,0 +1,23 @@
+BEGIN;
+
+CREATE TABLE privacy_groups (
+  "id"                        TEXT            NOT NULL,
+  "domain"                    TEXT            NOT NULL,
+  "schema_id"                 TEXT            NOT NULL,
+  "schema_signature"          TEXT            NOT NULL,
+  "created"                   BIGINT          NOT NULL,
+  "originator"                TEXT            NOT NULL,
+  PRIMARY KEY ( "domain", "id" )
+);
+CREATE INDEX privacy_groups_created ON privacy_groups ("created");
+CREATE INDEX privacy_groups_schema_id ON privacy_groups ("schema_id");
+
+CREATE TABLE privacy_group_members (
+    "domain"      TEXT    NOT NULL,
+    "group"       TEXT    NOT NULL,
+    "identity"    TEXT    NOT NULL,
+    PRIMARY KEY ("domain", "group", "identity"),
+    FOREIGN KEY ("domain", "group") REFERENCES privacy_groups ("domain", "id") ON DELETE CASCADE
+);
+
+COMMIT;
