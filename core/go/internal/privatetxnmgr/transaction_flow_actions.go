745db721558fc5f2c907c2246a3fe76f816bfe6e
Jim Zhang <jim.zhang@kaleido.io>
2025-04-24 16:15:15 -0400

    deduplicate resolve verifier requests
    Signed-off-by: Jim Zhang <jim.zhang@kaleido.io>

--- core/go/internal/privatetxnmgr/transaction_flow_actions.go
+++ core/go/internal/privatetxnmgr/transaction_flow_actions.go
@@ -653,6 +653,9 @@ func (tf *transactionFlow) requestVerifierResolution(ctx context.Context) {
 	if tf.transaction.PreAssembly.Verifiers == nil {
 		tf.transaction.PreAssembly.Verifiers = make([]*prototk.ResolvedVerifier, 0, len(tf.transaction.PreAssembly.RequiredVerifiers))
 	}
+	// having duplicate requests for the same verifier can cause the same transaction to be sent multiple times
+	tf.transaction.PreAssembly.RequiredVerifiers = dedupResolveVerifierRequests(tf.transaction.PreAssembly.RequiredVerifiers)
+
 	for _, v := range tf.transaction.PreAssembly.RequiredVerifiers {
 		tf.logActionDebugf(ctx, "Resolving verifier %s", v.Lookup)
 		tf.identityResolver.ResolveVerifierAsync(
@@ -671,4 +674,17 @@ func (tf *transactionFlow) requestVerifierResolution(ctx context.Context) {
 	}
 	//TODO this needs to be more precise (like which verifiers have been sent / pending / stale  etc)
 	tf.requestedVerifierResolution = true
+}
+
+func dedupResolveVerifierRequests(requests []*prototk.ResolveVerifierRequest) []*prototk.ResolveVerifierRequest {
+	seen := make(map[string]struct{})
+	var dedupedRequests []*prototk.ResolveVerifierRequest
+	for _, request := range requests {
+		key := fmt.Sprintf("%s:%s:%s", request.Lookup, request.VerifierType, request.Algorithm)
+		if _, ok := seen[key]; !ok {
+			seen[key] = struct{}{}
+			dedupedRequests = append(dedupedRequests, request)
+		}
+	}
+	return dedupedRequests
 }
