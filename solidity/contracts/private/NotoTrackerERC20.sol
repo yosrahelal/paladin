a75d6632de9654eda278e1919c041df17e82769b
Andrew Richardson <andrew.richardson@kaleido.io>
2025-05-27 17:43:12 -0400

    Add lock handling to NotoTrackerPublicERC20
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- solidity/contracts/private/NotoTrackerERC20.sol
+++ solidity/contracts/private/NotoTrackerERC20.sol
@@ -10,7 +10,7 @@ import {NotoLocks} from "./NotoLocks.sol";
  * @dev Example Noto hooks which track all Noto token movements on a private ERC20.
  */
 contract NotoTrackerERC20 is INotoHooks, ERC20 {
-    NotoLocks internal _locks;
+    NotoLocks internal _locks  = new NotoLocks();
     address internal _notary;
 
     modifier onlyNotary(address sender) {
@@ -32,7 +32,6 @@ contract NotoTrackerERC20 is INotoHooks, ERC20 {
     }
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        _locks = new NotoLocks();
         _notary = msg.sender;
     }
 
