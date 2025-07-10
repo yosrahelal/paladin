5ab7de3db3952e9ff25d8ba080d7aea2fd828219
Andrew Richardson <andrew.richardson@kaleido.io>
2025-07-10 09:58:28 -0400

    sdk: add "split" for splitting Paladin identifiers into identity/node
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- sdk/typescript/src/verifier.ts
+++ sdk/typescript/src/verifier.ts
@@ -8,6 +8,11 @@ export class PaladinVerifier {
     return this.lookup;
   }
 
+  split() {
+    const [identity, node] = this.lookup.split("@");
+    return { 0: identity, 1: node, identity, node };
+  }
+
   resolve(algorithm: Algorithms, verifierType: Verifiers) {
     return this.paladin.ptx.resolveVerifier(
       this.lookup,
