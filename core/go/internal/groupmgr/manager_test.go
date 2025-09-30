be068e6248b121f8b81298efbf4989a7c1401c41
Anna McAllister <anna.mcallister@kaleido.io>
2025-09-30 10:25:00 +0100

    add mock
    Signed-off-by: Anna McAllister <anna.mcallister@kaleido.io>

--- core/go/internal/groupmgr/manager_test.go
+++ core/go/internal/groupmgr/manager_test.go
@@ -107,6 +107,7 @@ func newTestGroupManager(t *testing.T, realDB bool, conf *pldconf.GroupManagerCo
 	logrus.SetLevel(logrus.TraceLevel)
 
 	mc := newMockComponents(t, realDB)
+	mc.domain.On("FixedSigningIdentity").Return("")
 	for _, fn := range extraSetup {
 		fn(mc, conf)
 	}
